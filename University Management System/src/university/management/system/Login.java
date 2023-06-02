3b783281c1a44d2a7ae9af5d2c60f6d3b8f83969
Harshal_SG <harshalsg550287@gmail.com>
2023-06-02 09:52:42 +0530

    Initial Commit

---
+++ University Management System/src/university/management/system/Login.java
@@ -0,0 +1,14 @@
+package university.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame{
+    Login(){
+        getContentPane().setBackground(Color.WHITE);
+        JLabel 
+        setSize(600,300);
+        setLocation(500,250);
+        setVisible(true);
+    }
+}
